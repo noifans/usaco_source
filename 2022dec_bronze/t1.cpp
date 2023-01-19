5809e1ba94accd741caaeddee5070567f8637451
noifans <92096738+noifans@users.noreply.github.com>
2023-01-19 20:18:05 +0800

    Update t1.cpp

--- 2022dec_bronze/t1.cpp
+++ 2022dec_bronze/t1.cpp
@@ -1,22 +1,28 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int a[100001];
+
+const int N=1e5+5;
+int a[N];  // cow 
+
 int main() {
-	int n,ans=0;
-	long long cnt =0;
-	cin >> n;
-	for(int i=0;i<n;i++) {
-		cin >> a[i];
-	}
-	sort(a,a+n);
-	for(int i=0;i<n;i++) {
-		long long t = 1LL*(n-i)*a[i];
-		if( t > cnt)  {
-			cnt = t;
-			ans = a[i];	
-		} 
-	}
-	cout << cnt <<" "<<ans;
-	return 0;
+        //freopen("t1_data/6.in","r",stdin);
+        int n;
+        cin >> n;
+        for(int i=0;i<n;i++) {
+                cin >> a[i];
+        }
+        sort(a,a+n);
+        int ans; // set money
+        long long total=0; // total 
+        for(int i=0;i<n;i++) {
+                long long t = 1LL*a[i]*(n-i);
+                if (t > total) {
+                        ans = a[i];
+                        total = t;
+                }
+        }
+        cout << total << " " << ans ;
+
+        return 0;
 }
